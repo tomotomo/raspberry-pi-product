c4bf0f3f4588bd3ba8c7f6016180d6cb7268460c
Takuya YOSHIMURA <yoshitaku-jp@users.noreply.github.com>
2018-12-02 15:11:05 +0900

    【update】hello_world

---
+++ hello_world/hello_world.js
@@ -0,0 +1,10 @@
+var sys = require('sys');
+var http = require('http');
+var server = http.createServer(
+    function (request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World!!');
+        response.end();
+    }
+).listen(3000);
+sys.log('Server running at http://localhost:3000/');
